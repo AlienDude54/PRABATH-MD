0f7b47261459961fbc31474cd14f377ccd53c81e
AlienDude54 <152387265+AlienDude54@users.noreply.github.com>
2024-07-09 15:42:59 +0100

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
+    SESSION_ID: PRABATH-MD~FZhG0YJB#j_POcAnSXaKAEeiVmob7SmzX6_GR1XtV1KYzbt4lH_s process.env.SESSION_ID,    
+    BOT_NUMBER: 2349130927388 process.env.BOT_NUMBER,
     GITHUB_USERNAME: username,
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
+    GITHUB_AUTH_TOKEN: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 process.env.GITHUB_AUTH_TOKEN,
 };
